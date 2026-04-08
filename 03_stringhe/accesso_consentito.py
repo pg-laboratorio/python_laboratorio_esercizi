f99d0a92f14cb103d0c0682f030bd4300f5af7af
pg23 <153715134+pg-laboratorio@users.noreply.github.com>
2026-04-08 16:53:35 +0200

    Add files via upload

---
+++ 03_stringhe/accesso_consentito.py
@@ -0,0 +1,4 @@
+'''
+CONSEGNA:
+Scrivi un programma che chieda all’utente l’username e la password, e mostri il messaggio “Accesso consentito” nel caso che l’user sia “admin” e la password “secure123".
+'''
