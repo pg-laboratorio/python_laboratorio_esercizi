585ad7fc0c2c7202603148c6cd799226c15a446b
pg-laboratorio <p.gardoni@yahoo.com>
2026-04-14 09:27:15 +0000

    Aggiunta es ecommerce

---
+++ 03_stringhe/codice_ecommerce.py
@@ -0,0 +1,4 @@
+'''
+Quando un utente acquista un oggetto sul tuo store, devi generare un codice per il corriere. Chiedi all'utente il nome dell'articolo acquistato e la città di destinazione. Crea un codice di spedizione composto dalle prime 3 lettere dell'articolo, le prime 3 lettere della città e il suffisso finale "-24H". Il codice intero deve essere scritto in maiuscolo.
+
+'''
