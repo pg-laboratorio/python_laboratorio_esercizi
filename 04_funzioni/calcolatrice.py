e91bee6162c3271930d0152f970c2717a8e2a8a9
pg-laboratorio <p.gardoni@yahoo.com>
2026-04-30 07:46:00 +0000

    agiornamento funzioni

--- 04_funzioni/calcolatrice.py
+++ 04_funzioni/calcolatrice.py
@@ -24,3 +24,59 @@ def divisione (a, b):
     else:
         div = a / b
         return div
+
+def mostra_menu():
+    """ Mostra le opzioni disponibili"""
+    print("\n")
+    print("-- Calcolatrice--")
+    print("+ : somma")
+    print("- : sottrazione")
+    print("* : moltiplicazione")
+    print("/ : divisione")
+    print("q : chiudere il programma")
+    print("\n")
+
+
+def calcolatrice():
+    """Gestisce il funzionamento di una calcolatrice"""
+    while True:
+        mostra_menu()
+
+        scelta = input("Inserisci l'operatore (+, -, *, /) oppure 'q' per chiudere: ")
+
+        if scelta == 'q':
+            print("Chiusura in corso...")
+            break
+
+        if scelta in ('+','-','*','/'):
+            #chiediamo i numeri
+            n1 = float(input("Inserisci il primo numero: "))
+            n2 = float(input("Inserisci il secondo numero: "))
+
+            if scelta == '+':
+                risultato = somma(n1,n2)
+                print(f"{n1} + {n2} = {risultato}")
+
+            elif scelta == '-':
+                risultato = differenza(n1,n2)
+                print(f"{n1} - {n2} = {risultato}")
+
+            elif scelta == '*':
+                risultato = moltiplicazione(n1,n2)
+                print(f"{n1} * {n2} = {risultato}")
+
+            elif scelta == '/':
+                risultato = divisione(n1,n2)
+                print(f"{n1} / {n2} = {risultato}")
+
+        else:
+            print("scelta non valida!")
+            continue #ritorno alla scelta
+
+
+calcolatrice()
+
+
+
+
+
